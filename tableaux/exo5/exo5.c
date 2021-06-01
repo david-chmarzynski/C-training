edd112f925c25939bad326b5b52760d768c263f1
David Chmarzynski <david.chm00@gmail.com>
2021-06-01 19:07:18 +0200

    exo5

--- tableaux/exo5/exo5.c
+++ tableaux/exo5/exo5.c
@@ -8,6 +8,15 @@
 
 void ordonnerTableau(int tableau[], int tailleTableau)
 {
+  int tmp = 0;
   for(int i = 0; i<tailleTableau; i++)
-  
+    for(int j = 0; j<tailleTableau; j++)
+    {
+      if(tableau[i] < tableau[j])
+      {
+        tmp = tableau[j];
+        tableau[j] = tableau[i];
+        tableau[i] = tmp;
+      }
+    }
 }
--- tableaux/exo5/main.c
+++ tableaux/exo5/main.c
@@ -4,6 +4,19 @@
 int main(void)
 {
 
+  int tableau[4] = { 15,81,22,13 };
+
+  // BEFORE
+  for(int i = 0; i<4; i++)
+    printf("%d", tableau[i]);
+  printf("\n");
+
+  ordonnerTableau(tableau, 4);
+
+  // AFTER
+  for(int i = 0; i<4; i++)
+    printf("%d", tableau[i]);
+  printf("\n");
 
   return 0;
 }
